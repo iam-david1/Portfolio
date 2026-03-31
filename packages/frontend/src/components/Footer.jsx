ff18b5067dce4921d7e3eb96cce7ef437df49a17
David Omwenyeke <omwenyekedavid07@gmail.com>
2026-03-31 09:48:53 +0100

    fix: add year to footer

--- packages/frontend/src/components/Footer.jsx
+++ packages/frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>Built and designed by David Omwenyeke. All rights reserved. &copy;</p>
+        <p>Built and designed by David Omwenyeke. All rights reserved. &copy; 2026</p>
       </div>
     </footer>
   );
